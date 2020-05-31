b3cb9d78d009ffa67158bda603f9e2f50d98f7f5
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-31 23:34:40 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,22 +2,22 @@ require 'pry'
 
 class String
 
-  def sentence?
-      if self.split && " ."
-        return true
-      end
+  class String
+
+    def sentence?
+      self.end_with?(".")
     end
 
-  def question?
+    def question?
+      self.end_with?("?")
+    end
 
+    def exclamation?
+      self.end_with?("!")
+    end
+
+    def count_sentences
+      sentence_count = self.squeeze("!")
+      sentence_count.delete(" ").split(/[.!?]/).length
+    end
   end
-
-  def exclamation?
-
-  end
-
-  def count_sentences
-
-  end
-
-end
